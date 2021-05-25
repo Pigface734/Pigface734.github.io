da51bf6071c2653b85b8c6982c0f3081f0c3565a
pigface <50749301+Pigface734@users.noreply.github.com>
2021-05-24 18:54:52 -0700

    Update timer.js

--- timer.js
+++ timer.js
@@ -12,7 +12,7 @@ function draw() {
   background(255)
   textSize(32);
   fill(0);
-  text((2028-year()+" years ")+ (8-month()+' months ')+(31-day()+" days"),250,50)
+  text((2028-year()+" years ")+ (8-month()+' months ')+(30-day()+" days"),250,50)
   text((23-hour()+" hours ")+(59-minute()+" minutes ")+(60-second()+" seconds"),250,100)
 
     //cant wait
